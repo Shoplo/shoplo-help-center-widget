6a4e11b9454ffc97ca50cf67aa7454bbe9a6aba2
rymopl <lukasz.riemer@gmail.com>
2019-10-09 14:33:39 +0200

    fix lang version

--- src/app/services/locales.service.ts
+++ src/app/services/locales.service.ts
@@ -5,41 +5,50 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 @Injectable()
 export class LocalesService {
   public localData: any;
-  constructor(
-    private activatedRoute: ActivatedRoute
-  ) {
-
-    if ( this.getParameterByName('locale') === 'en') {
-      this.localData = environment.widget_settings_en;
-    } else {
+  constructor(private activatedRoute: ActivatedRoute) {
+    if (this.getParameterByName('locale') === 'pl_PL') {
       this.localData = environment.widget_settings_pl;
+    } else {
+      this.localData = environment.widget_settings_en;
     }
 
-    if ( this.getParameterByName('product') === 'store') {
+    if (this.getParameterByName('product') === 'store') {
       this.localData.product = 'Shoplo Store';
     }
 
-    if ( this.getParameterByName('product') === 'multi') {
+    if (this.getParameterByName('product') === 'multi') {
       this.localData.product = 'Shoplo Multichannel';
     }
 
-    if ( this.getParameterByName('product') === 'shoplo') {
+    if (this.getParameterByName('product') === 'shoplo') {
       this.localData.product = 'Shoplo';
     }
 
-    if ( this.getParameterByName('product') === 'mobile' && this.getParameterByName('locale') === 'en') {
+    if (
+      this.getParameterByName('product') === 'mobile' &&
+      this.getParameterByName('locale') === 'en'
+    ) {
       this.localData.product = 'Mobile app';
     }
 
-    if ( this.getParameterByName('product') === 'mobile' && this.getParameterByName('locale') === 'pl') {
+    if (
+      this.getParameterByName('product') === 'mobile' &&
+      this.getParameterByName('locale') === 'pl'
+    ) {
       this.localData.product = 'Aplikacja Mobilna';
     }
 
-    if ( this.getParameterByName('product') === 'chat' && this.getParameterByName('locale') === 'en') {
+    if (
+      this.getParameterByName('product') === 'chat' &&
+      this.getParameterByName('locale') === 'en'
+    ) {
       this.localData.product = 'Shoplo Chat';
     }
 
-    if ( this.getParameterByName('product') === 'chat' && this.getParameterByName('locale') === 'pl') {
+    if (
+      this.getParameterByName('product') === 'chat' &&
+      this.getParameterByName('locale') === 'pl'
+    ) {
       this.localData.product = 'Shoplo Czat';
     }
 
@@ -50,24 +59,29 @@ export class LocalesService {
     const url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$?');
     const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
+      results = regex.exec(url);
 
-    if (!results) {return null; }
+    if (!results) {
+      return null;
+    }
 
-    if (!results[2]) {return ''; }
+    if (!results[2]) {
+      return '';
+    }
 
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
 
-  private makeNonce () {
-      let text = '',
-          i;
-      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  private makeNonce() {
+    let text = '',
+      i;
+    const possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-      for (i = 0; i < 10; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
+    for (i = 0; i < 10; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
 
-      return text;
+    return text;
   }
 }
