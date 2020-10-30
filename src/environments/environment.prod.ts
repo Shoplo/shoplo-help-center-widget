cb7f1a54a594136cd91138b0b143831400b85458
giedoka <pasiak5@gmail.com>
2020-10-30 14:50:10 +0100

    comments removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,9 +8,6 @@ export const environment = {
       apiKey: '1e0c6b677c9197f3efdcc7936c820c2d',
       appId: 'XW19CXV180',
       indexName: 'help_center_en_posts_post',
-      // apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-      // appId: 'latency',
-      // indexName: 'instant_search',
       urlSync: false
     }
   },
@@ -21,9 +18,6 @@ export const environment = {
       apiKey: '1e0c6b677c9197f3efdcc7936c820c2d',
       appId: 'XW19CXV180',
       indexName: 'help_center_pl_posts_post'
-            // apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-      // appId: 'latency',
-      // indexName: 'instant_search',
     }
   },
   widget_settings_en_gb: {
